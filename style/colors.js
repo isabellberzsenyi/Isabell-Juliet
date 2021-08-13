703a962ebf565ff2a4cc905dbb53a999b5f2ede6
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2021-08-12 23:34:23 -0400

    Importing Noto Sans typography and colors

---
+++ style/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  BLUSH: '#F2E6DF',
+  MUD: '#59382C',
+  CHOCOLATE: '#734D45',
+  ALMOND: '#A6837B',
+  RASPBERRY: '#732020',
+};
+
+export { colors };
---
+++ style/typography.js
@@ -0,0 +1,11 @@
+const fonts = {
+  notoSans: 'Noto Sans',
+};
+
+const fontWeights = {
+  normal: 400,
+  medium: 500,
+  bold: 700,
+};
+
+export { fonts, fontWeights };
